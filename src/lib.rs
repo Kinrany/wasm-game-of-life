6a2c046db3c2af3ddaaaffaf6084a0c1d79c6650
Kinrany <kinrany@yandex.com>
2019-05-11 21:06:30 +0300

    Expose universe directly instead of as a string

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,35 @@ pub struct Universe {
 
 #[wasm_bindgen]
 impl Universe {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn set_cell(&mut self, row: u32, column: u32, value: Cell) {
+        let idx = self.get_cell_index(row, column);
+        self.cells[idx] = value;
+    }
+
+    pub fn new(width: u32, height: u32) -> Universe {
+        let cells = (0..width * height)
+            .map(|i| { if i % 3 == 0 || i % 7 == 0 { Cell::Alive } else { Cell::Dead } })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells
+        }
+    }
+
     pub fn tick(&mut self) {
         let mut next_cells = self.cells.clone();
 
@@ -83,22 +112,6 @@ impl Universe {
             }
         }
         count
-    }
-
-    pub fn new(width: u32, height: u32) -> Universe {
-        let cells = (0..width * height)
-            .map(|i| { if i % 3 == 0 || i % 7 == 0 { Cell::Alive } else { Cell::Dead } })
-            .collect();
-
-        Universe {
-            width,
-            height,
-            cells
-        }
-    }
-
-    pub fn render(&self) -> String {
-        self.to_string()
     }
 }
 
