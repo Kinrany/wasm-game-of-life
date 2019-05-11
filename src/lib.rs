d91664ff316db35405732c386e462d0e5499eba1
Kinrany <kinrany@yandex.com>
2019-05-11 20:25:31 +0300

    Replace hello world with a Game of Life simulation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
-use wasm_bindgen::prelude::*;
+use {
+    wasm_bindgen::prelude::*,
+    std::fmt,
+};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +12,109 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+fn rules(cell: &Cell, alive_neighbors: u8) -> Cell {
+    match (cell, alive_neighbors) {
+        // Rule 1: Any live cell with fewer than two live neighbours
+        // dies, as if caused by underpopulation.
+        (Cell::Alive, x) if x < 2 => Cell::Dead,
+        // Rule 2: Any live cell with two or three live neighbours
+        // lives on to the next generation.
+        (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+        // Rule 3: Any live cell with more than three live
+        // neighbours dies, as if by overpopulation.
+        (Cell::Alive, x) if x > 3 => Cell::Dead,
+        // Rule 4: Any dead cell with exactly three live neighbours
+        // becomes a live cell, as if by reproduction.
+        (Cell::Dead, 3) => Cell::Alive,
+        // All other cells remain in the same state.
+        (cell, _) => cell.clone(),
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next_cells = self.cells.clone();
+
+        for row in 0..self.height {
+            for column in 0..self.width {
+                let idx = self.get_cell_index(row, column);
+                let cell = self.cells[idx];
+                let live_neighbor_count = self.alive_neighbor_count(row, column);
+
+                let next_cell = rules(&cell, live_neighbor_count);
+                next_cells[idx] = next_cell;
+            }
+        }
+
+        self.cells = next_cells;
+    }
+
+    fn get_cell_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn alive_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_column in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_column == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_column = (column + delta_column) % self.width;
+                let idx = self.get_cell_index(neighbor_row, neighbor_column);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    pub fn new(width: u32, height: u32) -> Universe {
+        let cells = (0..width * height)
+            .map(|i| { if i % 3 == 0 || i % 7 == 0 { Cell::Alive } else { Cell::Dead } })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = match cell {
+                    Cell::Dead => '◻',
+                    Cell::Alive => '◼',
+                };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
 }
